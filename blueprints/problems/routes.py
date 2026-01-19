80ddf94e6c6b93446177d3cf6ef75d3fe0d70326
RRRaiden <iliya.atmeneev@gmail.com>
2026-01-19 21:17:22 +0300

    Вывод списка задач

--- blueprints/problems/routes.py
+++ blueprints/problems/routes.py
@@ -1,19 +1,75 @@
 """СКРИПТ:Пути для отображения задач пользователям"""
 
 # -- импорт модулей
-import datetime
-from flask import current_app
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, current_app
+from flask import request
 
-template_dir = current_app.config['TEMPLATE_PATH']
-bp = Blueprint('problems', __name__, template_folder=template_dir)
+from models import Problem
+
+bp = Blueprint('problems', __name__, template_folder='templates')
 
 
-@bp.route('/', methods=['GET'])
+@bp.route('/', methods=['GET',])
 def problems():
-    return render_template('problems.html')
+
+    # Получаем текущую страницу из параметров запроса, по умолчанию страница 1
+    page = request.args.get('page', 0, type=int)
+    sort = request.args.get('sort', 'id')
+
+    # Устанавливаем количество элементов на одной странице
+    per_page = current_app.config['PROBLEMS_PAGINATION_ADMIN']
+
+    problem_query = Problem.query
+    # Общее количество элементов
+    total_items = problem_query.count()
+
+    last_page = (total_items - 1) // per_page
+
+    has_next = page < last_page
+    has_prev = page > 0
+    if total_items == 0:
+        return render_template(
+            'problems.html',
+            problems=[],
+            current_page=0,
+            current_sort=sort,
+            has_next=False,
+            has_prev=False
+        )
+
+    last_page = (total_items - 1) // per_page
+
+    if page > last_page:
+        return redirect(url_for('problems.problems', page=last_page, sort=sort))
+
+    if page < 0:
+        page = 0
+
+    problem_list = (
+        problem_query
+        .limit([per_page])
+        .offset(page * per_page)
+        .all()
+    )
+
+    # Общее количество страниц (округляем вверх)
+    total_pages = (total_items + per_page - 1) // per_page
+
+    # Определяем начальный и конечный индекс для текущей страницы
+    has_next = page < last_page
+    has_prev = page > 0
+
+    return render_template(
+        'problems.html',
+        problems=problem_list,
+        current_page=page,
+        current_sort=sort,
+        has_next=has_next,
+        has_prev=has_prev
+    )
 
 
 @bp.route('/<problem_id>', methods=['GET'])
 def problem():
-    return render_template('problem.html')
+    tasks = Problem.query.filter(Problem.id == Problem.id).all()
+    return render_template('problem.html', tasks=tasks)
