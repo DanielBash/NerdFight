2cc9131ec88129ea9ba988b58792013f8a812065
Xucot <xucot00@gamail.com>
2026-01-23 17:42:35 +0300

    users

--- blueprints/users/routes.py
+++ blueprints/users/routes.py
@@ -4,7 +4,8 @@
 import datetime
 from flask import current_app
 from flask import Blueprint, render_template
-
+import sqlite3
+from models import User
 
 template_dir = current_app.config['TEMPLATE_PATH']
 bp = Blueprint('users', __name__, template_folder=template_dir)
@@ -12,7 +13,9 @@ bp = Blueprint('users', __name__, template_folder=template_dir)
 
 @bp.route('/', methods=['GET'])
 def users():
-    return render_template('users.html')
+    users_data = User.query.order_by(User.elo).all()
+    users_data = zip(range(1, 100), users_data)
+    return render_template('users.html', users_data=users_data)
 
 @bp.route('/<string:username>', methods=['GET'])
 def user(username):
